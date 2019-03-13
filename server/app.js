4e96ba018ab0d0646b3a72fbfefb684934e8edf9
Felix <felixgohh08@gmail.com>
2019-03-13 13:18:48 +0700

    use mongodb atlas as server

--- server/app.js
+++ server/app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express'),
     app = express(),
     port = 3000,
@@ -5,7 +7,7 @@ const express = require('express'),
     articleRoute = require('./routes/articleRoute'),
     cors = require('cors')
 
-mongoose.connect('mongodb://localhost/miniwp', { useNewUrlParser: true })
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-s6ej4.gcp.mongodb.net/test2?retryWrites=true`, { useNewUrlParser: true })
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
